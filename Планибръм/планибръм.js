63a2dfca7123027e47fcb84d0fec1f70fd172a38
RukiDuki <vaneska123450@gmail.com>
2026-06-01 10:49:20 +0300

    I FIGURED OUT STASHING!!! (experimenting with the dropdown)

--- Планибръм/планибръм.js
+++ Планибръм/планибръм.js
@@ -1,3 +1,12 @@
 function changeImage() {
+<<<<<<< Updated upstream
         document.getElementById("img").src=".jpg";
     }// JavaScript Document
+=======
+            var color = getElementById("colorSelect");
+            const img = document.getElementById("stereo");
+            //check the element's value
+            if (color.id === "white"){
+                img.src = "../снимки/бяло.jpg";
+			}
+>>>>>>> Stashed changes
